19c9608bfa0dedc2f9dffc1148ad6a87c036e17b
tristan-scott <scottt@uleth.ca>
2023-11-26 16:12:42 -0700

    things

--- js/script.js
+++ js/script.js
@@ -1,8 +1,1 @@
-function changeItem(itemId) {
-    // Get the item and character image elements
-    var item = document.getElementById(itemId);
-    var characterImage = document.getElementById("characterImage");
-
-    // Set the character image src to the selected item's src
-    characterImage.src = item.src;
-}
+console.log('CONNECTED')
